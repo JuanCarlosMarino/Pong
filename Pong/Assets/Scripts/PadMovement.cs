082c1aa90febca50ef68bc43843a4391ae8c9b38
JuanCarlosMarino <juan.marino.2017@upb.edu.co>
2020-08-12 11:54:25 -0500

    Cambios clase 12 Agosto

--- Pong/Assets/Scripts/PadMovement.cs
+++ Pong/Assets/Scripts/PadMovement.cs
@@ -5,24 +5,40 @@ using UnityEngine;
 /// <summary>
 /// Moves the player alongside the y axis
 /// </summary>
+[RequireComponent(typeof(Rigidbody2D))]
 public class PadMovement : MonoBehaviour
 {
     //movement velocity
     [Tooltip("Velocity in unity units!")]
     [SerializeField] private float velocity = 5f;
+
+    [Header("Controles para GamePad:")]
+    [SerializeField] private KeyCode UpControl; 
+    [SerializeField] private KeyCode DownControl; 
+
+    private Rigidbody2D _rigidbody2D;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //leer el control        
+        //leer el control de las teclas        
         //aplicar movimiento
         //mover hacia arriba
-        if(Input.GetAxis("Vertical") > 0)
+        if(Input.GetKey(UpControl))
             transform.Translate(x: 0f,y: velocity,z: 0f);
+        //mover hacia abajo
+        else if(Input.GetKey(DownControl))
+            transform.Translate(x: 0f,y: -velocity,z: 0f);
+        if(!_rigidbody2D.Equals(other: null))
+            _rigidbody2D.AddForce(Vector2.up,ForceMode2D.Impulse);
+        else
+        {
+            Debug.LogWarning(message: "El objeto no tiene rigidbody!!!");
+        } 
     }
 }
